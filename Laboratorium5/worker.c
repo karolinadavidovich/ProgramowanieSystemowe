eaa680417c932892ef5467dcc0a5f808b8794985
karolinadavidovich <155163711+karolinadavidovich@users.noreply.github.com>
2025-11-26 16:35:18 +0100

    Add files via upload

---
+++ Laboratorium5/worker.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "worker.h"
+
+void worker_loop(int read_fd, int write_fd)
+{
+	char str[50];
+
+	while(1)
+	{
+		read(read_fd, str, sizeof(str));
+
+		if (strcmp(str, "exit") == 0)
+			return;
+
+		str[0] = 'X';
+
+		write(write_fd, str, strlen(str) + 1);
+
+	}
+}
---
+++ Laboratorium5/worker.h
@@ -0,0 +1,6 @@
+#ifdef WORKER_H
+#define WORKER_H
+
+void worker_loop(int read_fd, int write_fd);
+
+#endif
